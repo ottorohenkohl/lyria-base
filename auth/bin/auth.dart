50def760399911da937193707fb0bafaf6781da8
Otto Rohenkohl <ottorohenkohl@icloud.com>
2022-07-14 18:04:13 +0200

    Everything's perfect now?

--- auth/bin/auth.dart
+++ auth/bin/auth.dart
@@ -5,6 +5,7 @@ import 'package:auth/config/config_manager.dart';
 import 'package:auth/console/console_manager.dart';
 import 'package:auth/session/session/session.dart';
 import 'package:auth/user/user/user.dart';
+import 'package:auth/user/user_manager.dart';
 import 'package:auth/user/user_role/user_role.dart';
 import 'package:hive/hive.dart';
 
@@ -16,8 +17,8 @@ void main(List<String> arguments) {
   Hive.registerAdapter(UserAdapter());
   Hive.registerAdapter(UserRoleAdapter());
 
-  print(Directory.current);
   autoConfig();
+  autoUser();
 
   ConsoleManager().handle(arguments);
 }
@@ -34,4 +35,21 @@ void autoConfig() async {
     ..apiHost = apiHost
     ..apiPath = apiPath
     ..save();
+}
+
+void autoUser() async {
+  var environment = Platform.environment;
+
+  String? username = environment['USER_USERNAME'];
+  String? password = environment['USER_PASSWORD'];
+  String? role = environment['USER_ROLE'];
+
+  if (username != null) {
+    var userRole = UserRole.admin;
+    if (role == 'admin') {
+      userRole == UserRole.admin;
+    }
+    var user = User(username: username, password: password, role: userRole);
+    UserManager().add(user);
+  }
 }
